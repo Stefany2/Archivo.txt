295a17d86aef0c95d02add2823f1c6b2ed7c6247
Stefany2 <1459565@senati.pe>
2023-02-23 18:21:43 -0500

    txt en python

---
+++ Escribir.py
@@ -0,0 +1,8 @@
+#Escribir archivo 
+#txt en pyton
+#Use \n para separar mi txt
+
+with open ("Nuevo.txt","a")as file_object: #Especificamospython
+    file_object.write("\nHola mi nombre es Camila\n") #Escribir 
+    file_object.write("y tengo 24 años") # en el archivo
+    
